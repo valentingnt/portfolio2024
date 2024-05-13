95e37a9ae46c301791cb2aa03a9b19fd2791a28c
valentingnt <valentin64.genest@gmail.com>
2024-05-13 05:01:25 +0200

    Remove unused code and update metadata in app.vue and index.vue

--- app.vue
+++ app.vue
@@ -50,7 +50,17 @@ import Lenis from 'lenis'
 //     },
 //   },
 // }
-
+useSeoMeta({
+  title: 'Valentin Genest',
+  description: 'Valentin Genest - Web developer for 5 years. Started from the bottom with wordpress, now at the top working with cutting-edge front-end related technology. I worked for 2 years for @derniercri and now in freelance accompanied by @studiowawww for biggest dreams. I love what I do and do it the right way. The stack I use differs depending on the project and needs, but I keep learning new things to build an internet as it should be.',
+  ogDescription: 'Valentin Genest - Web developer for 5 years. Started from the bottom with wordpress, now at the top working with cutting-edge front-end related technology. I worked for 2 years for @derniercri and now in freelance accompanied by @studiowawww for biggest dreams. I love what I do and do it the right way. The stack I use differs depending on the project and needs, but I keep learning new things to build an internet as it should be.',
+  ogTitle: 'Valentin Genest',
+  ogType: 'website',
+  ogUrl: 'https://valentingenest.fr',
+  ogImage: '/img/moi.jpeg',
+  ogImageAlt: 'Valentin Genest',
+  ogSiteName: 'Valentin Genest',
+})
 
 const lenis = ref<Lenis>()
 
