790e63b5f52a5c5b86e0271ea7a942d8a864aa47
valentingnt <valentin64.genest@gmail.com>
2024-05-19 17:42:33 +0200

    refactor: Remove unused code and comments in app.vue

--- app.vue
+++ app.vue
@@ -1,55 +1,6 @@
 <script setup lang="ts">
-// import type { FooterData } from '~/components/layout/Footer.vue';
-// import type { NavbarData } from '~/components/layout/Navbar.vue';
-
 import Lenis from 'lenis'
 
-// interface LayoutData {
-//   navbar: NavbarData
-//   footer: FooterData
-// }
-
-// const MOCK_DATA: LayoutData = {
-//   navbar: {
-//     links: [
-//       {
-//         text: 'home',
-//         href: '/',
-//       },
-//       {
-//         text: 'projects',
-//         href: '/projects',
-//       },
-//       {
-//         text: 'about_me',
-//         href: '/about',
-//       },
-//     ],
-//   },
-//   footer: {
-//     title: 'Valentin Genest',
-//     links: [
-//       {
-//         title: 'home',
-//         href: '/',
-//       },
-//       {
-//         title: 'projects',
-//         href: '/projects',
-//       },
-//       {
-//         title: 'about_me',
-//         href: '/about',
-//       },
-//     ],
-//     contactText: 'contact:',
-//     mailto: 'valentin64.genest@gmail.com',
-//     legalTexts: {
-//       allRightsReserved: 'Valentin Genest — Tous droits réservés.',
-//       developedBy: 'Design et développement par Valentin Genest',
-//     },
-//   },
-// }
 useSeoMeta({
   title: 'Valentin Genest',
   description: 'Valentin Genest - Web developer for 5 years. Now at the top working with cutting-edge front-end related technology. I worked for 2 years for @derniercri and now in freelance accompanied by @studiowawww for biggest dreams. I love what I do and do it the right way. The stack I use differs depending on the project and needs, but I keep learning new things to build an internet as it should be.',
