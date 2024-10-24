1b6ffa7e1098ec5caf0c189e7217a25a234a20bc
Valentin Genest <valentin64.genest@gmail.com>
2024-10-24 18:07:03 +0200

    update useHead on base page

--- pages/index.vue
+++ pages/index.vue
@@ -11,6 +11,29 @@ onMounted(() => {
   }
   router.push(`/${cookieLang.value}`)
 })
+
+useHead(() => ({
+  htmlAttrs: {
+    lang: cookieLang.value,
+  },
+  title: 'Valentin Genest - Développeur Front-end',
+  meta: [
+    {
+      name: 'description',
+      content: 'Valentin Genest - Développeur front-end avec 5 ans d\'expérience. Spécialisé dans la création d\'expériences web de qualité. Construisons un internet comme il se doit.',
+    },
+    {
+      property: 'og:description',
+      content: 'Valentin Genest - Développeur front-end. Construisons un internet comme il se doit.',
+    },
+    { property: 'og:title', content: 'Valentin Genest' },
+    { property: 'og:type', content: 'website' },
+    { property: 'og:url', content: `https://valentingenest.fr` },
+    { property: 'og:image', content: '/img/ogImage.jpg' },
+    { property: 'og:image:alt', content: 'Valentin Genest' },
+    { property: 'og:site_name', content: 'Valentin Genest' },
+  ],
+}))
 </script>
 
 <template>
